43cb9197555d5553d2289df8092247c296fbe385
Valerka <76082412+valerka03@users.noreply.github.com>
2021-07-21 21:42:53 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+});
+modal.addEventListener('click',(e)=>{
+  if(e.target=== modal){
+    
+  }
+});
